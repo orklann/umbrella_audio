f429be6f4d655f8668823ab78470a8f914d389a5
Tommy Jeff <threcius@yahoo.com>
2026-09-12 11:34:45 +0800

    Fixed compiler error

--- src/umbrella_audio.cr
+++ src/umbrella_audio.cr
@@ -1,6 +1,6 @@
 @[Link(ldflags: "-L#{__DIR__}/../ext/dist/release -lAudioTee -framework AudioToolbox -framework CoreAudio -framework Foundation")]
 lib Native
-  fun audiotee_permission_request(Void) : Void
+  fun audiotee_permission_request : Void
   fun audiotee_test : Void
   fun audiotee_start(
       include_pids : Int32*,
